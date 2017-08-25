38533c6dd143120a58d95e22c3447c998f582dc4
Fred Carlsen <fred@sjelfull.no>
2017-08-25 10:14:10 +0200

    Add console command to pretransform folder/source

--- consolecommands/ImagerPretransformCommand.php
+++ consolecommands/ImagerPretransformCommand.php
@@ -17,7 +17,58 @@ class ImagerPretransformCommand extends BaseCommand
 {
     /**
      */
-    public function actionIndex($param="")
+    public function actionIndex ($sourceHandle = null, $folderId = null)
     {
+        $assets = [ ];
+
+        if ( empty($sourceHandle) && empty($folderId) ) {
+            echo "No source handle or folderId was specified";
+            exit(0);
+        }
+
+        if ( !empty($sourceHandle) ) {
+            $sources = craft()->assetSources->getAllSources();
+            $source  = null;
+
+            foreach ($sources as $sourceCheck) {
+                if ( $sourceCheck->handle === $sourceHandle ) {
+                    $source = $sourceCheck;
+                    break;
+                }
+            }
+
+            if ( $source ) {
+                $assets = craft()->assets->getFilesBySourceId($source->id);
+            }
+        }
+
+        if ( !empty($folderId) ) {
+            $assets = $this->getFilesFromFolderId($folderId);
+        }
+
+        if ( empty($assets) ) {
+            echo "No assets found";
+            exit(0);
+        }
+
+        foreach ($assets as $asset) {
+            craft()->imagerPretransform->transformAsset($asset);
+        }
+
+        echo "Done.";
+        exit(0);
+    }
+
+    private function getFilesFromFolderId ($folderId = null)
+    {
+        $files = craft()->db->createCommand()
+                            ->select('fi.*')
+                            ->from('assetfiles fi')
+                            ->join('assetfolders fo', 'fo.id = fi.folderId')
+                            ->where('fi.folderId = :folderId', array( ':folderId' => $folderId ))
+                            ->order('fi.filename')
+                            ->queryAll();
+
+        return AssetFileModel::populateModels($files, $indexBy = null);
     }
 }
